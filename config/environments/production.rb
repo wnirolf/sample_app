a54667a791766f0ee178d17babe29b60063080da
wnirolf <wnirolf@hotmail.com>
2012-11-08 09:02:59 +0200

    Finish user signup

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -70,9 +70,7 @@ SampleApp::Application.configure do
   # Force all access to the app over SSL, use Strict-Transport-Security, 
   # and use secure cookies.
   config.force_ssl = true
-  .
-  .
-  .
+  
 end
   
   
